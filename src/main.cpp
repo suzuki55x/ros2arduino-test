dca68ef315fca5980f945667aaa0a2fa14b52397
suzuki_ta <suzuki_ta@alco-ex.jp>
2021-01-18 19:08:58 +0900

    update 暫定

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,47 @@
-#include <Arduino.h>
+#include <M5Stack.h>
+#include <ros2arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define XRCEDDS_PORT Serial
+
+bool is_enable = false;
+
+void subscribeLed(std_msgs::Bool *msg, void *arg)
+{
+  (void)(arg);
+
+  is_enable = !is_enable;
+  //digitalWrite(LED_BUILTIN, msg->data);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+class LedSub : public ros2::Node
+{
+public:
+  LedSub()
+      : Node("ros2arduino_sub_node")
+  {
+    this->createSubscriber<std_msgs::Bool>("chatter", (ros2::CallbackFunc)subscribeLed, nullptr);
+  }
+};
+
+void setup()
+{
+  XRCEDDS_PORT.begin(115200);
+  while (!XRCEDDS_PORT)
+    ;
+
+  ros2::init(&XRCEDDS_PORT);
+}
+
+void loop()
+{
+  static LedSub LedNode;
+
+  M5.Lcd.setCursor(10, 100);
+  if (is_enable) {
+    M5.Lcd.print("hoge");
+  } else {
+    M5.Lcd.print("fuga");
+  }
+
+  ros2::spin(&LedNode);
 }
